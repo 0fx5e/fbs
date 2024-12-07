1423b907ee1859a0efe468946643d66f58ec1a0f
Noob Cypher <zishin.dev@gmail.com>
2024-12-08 05:21:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cors = require('cors');
-const axios = require('axios');
-const fs = require('fs/promises');
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import cors from 'cors';
+import axios from 'axios';
+import { promises as fs } from 'fs';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 5000;
